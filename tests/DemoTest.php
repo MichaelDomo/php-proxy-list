9b045298e3df62fafe6375ea2f77b2272306e6b0
Domolazov Michael <misha_domolazov@mail.ru>
2017-03-27 22:49:48 +0300

    dev

---
+++ tests/DemoTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace michaeldomo\proxylist\tests;
+
+class DemoTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
